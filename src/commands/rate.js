c6c383e3c8b760265d940368a166df67744f156d
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-09-01 22:16:27 +1200

    Update rate.js

--- src/commands/rate.js
+++ src/commands/rate.js
@@ -2,7 +2,7 @@ const { rateDogeEur } = require('../requests')
 
 function rate (message) {
   rateDogeEur().then((rate) => {
-    message.channel.send('1 DGE = ' + rate + ' EUR')
+    message.channel.send('Sorry, the rate command is not working at the moment.')
   }).catch((error) => {
     message.channel.send(error)
   })
