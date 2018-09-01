5e03005d4ad037aace5c7193d3dc2e149b014699
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-09-01 21:53:51 +1200

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,16 +1,15 @@
 const { GIPHY_KEY } = require('../settings')
 
-const HELP_TEXT = '```Wow. DogeCoin to moon. \n \
+const HELP_TEXT = '```Flamecoin \n \
 Command list : \n \
-  - **help** : Get help ! \n \
-  - **tip** : Tip someone some dogecoin \n \
+  - **help** : Get help! \n \
+  - **tip** : Tip someone some flamecoin \n \
   - **balance** : Get your balance \n \
   - **rate** : Get current value \n \
   - **address** : Show an used wallet so you can refill your wallet \n \
   - **withdraw** : Move coins to an external address \n \
   - **qrcode** : Show your qrcode to receive much money \n \
-  - **voucher** : Get your dogecoins from your voucher card !! (Only work in private message with the bot) \n \
-' + ((GIPHY_KEY !== null && GIPHY_KEY !== '') ? '  - **goodboy** : Wow, much doggo, such reward, wow \n' : '') + ' \
+  - **voucher** : Get your flamecoins from your voucher card! (Only work in private message with the bot) \n \
 ```'
 
 function help(message) {
