2236411b79b4e94b9917e6078428bf41d0ae559f
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-09-01 21:29:10 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ client.on('message', message => {
   // If message has been emitted by a bot do nothing
   if (message.author.bot) return
 
-  if (message.content.startsWith('/fla')) {
+  if (message.content.startsWith('-')) {
     var args = message.content.substring(1).split(' ')
     var command = args[1]
 
