56f67d9fa96dd8fe99c482efb7e7f368ec911ea2
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-09-06 11:50:45 +1200

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,15 +1,15 @@
 const { GIPHY_KEY } = require('../settings')
 
-const HELP_TEXT = '```Flamecoin \n \
+const HELP_TEXT = '```Flamecoin Commands\n \
 Command list : \n \
-  - **help** : Get help! \n \
-  - **tip** : Tip someone some flamecoin \n \
-  - **balance** : Get your balance \n \
-  - **rate** : Get current value \n \
-  - **address** : Show an used wallet so you can refill your wallet \n \
-  - **withdraw** : Move coins to an external address \n \
-  - **qrcode** : Show your qrcode to receive much money \n \
-  - **voucher** : Get your flamecoins from your voucher card! (Only work in private message with the bot) \n \
+  - help : Get help! \n \
+  - tip : Tip someone some flamecoin \n \
+  - balance : Get your balance \n \
+  - rate : Get current value \n \
+  - address : Show an used wallet so you can refill your wallet \n \
+  - withdraw : Move coins to an external address \n \
+  - qrcode : Show your qrcode to receive much money \n \
+  - voucher : Get your flamecoins from your voucher card! (Only work in private message with the bot) \n \
 ```'
 
 function help(message) {
